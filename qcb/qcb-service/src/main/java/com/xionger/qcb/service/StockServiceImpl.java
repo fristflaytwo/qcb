c3fa6ffd59cc86ef17432c7dd8c291fd78cb1e12
fristflaytwo <fristflaytwo@yeah.net>
2016-11-25 17:46:21 +0800

    代码优化

--- qcb/qcb-service/src/main/java/com/xionger/qcb/service/StockServiceImpl.java
+++ qcb/qcb-service/src/main/java/com/xionger/qcb/service/StockServiceImpl.java
@@ -10,7 +10,6 @@ import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
